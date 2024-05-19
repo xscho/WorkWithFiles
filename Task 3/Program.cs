f76f3d951be72355e4a19ec03aa86ff5b0ede7e3
xscho <lina.che.19@bk.ru>
2024-05-19 13:23:28 +0300

    task

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -10,7 +10,7 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello! hkhgkig");
         }
     }
 }
